b10c0e95d84d53282c087e57c97f6e11bc2756bb
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-04-11 20:35:17 +0200

    feat(docs): add announcement bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,6 +97,11 @@ module.exports = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Papers AG - Open Source MIT License`,
     },
+    announcementBar: {
+      id: 'beta-docs',
+      content:
+        'This documentation is currently work in progress. Some information might be inaccurate.',
+    },
   },
   presets: [
     [
