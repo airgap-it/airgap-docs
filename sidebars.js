2acca7fa5cc94eeed5684c7d3b3e51ac62424850
debel damilola <44111962+debeldami@users.noreply.github.com>
2022-09-21 09:58:24 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -215,6 +215,7 @@ module.exports = {
         "guides/vault-offline-update",
         "guides/blue-wallet",
         "guides/sparrow-wallet",
+        "guides/interaction",
       ],
     },
     "FAQ",
