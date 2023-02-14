84fb9ad289793882bf62b668cffaf880d95653f1
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-02-14 13:47:24 +0100

    fix(): remove invalid sidebar link

--- sidebars.js
+++ sidebars.js
@@ -219,7 +219,6 @@ module.exports = {
         "guides/metamask-mobile",
         "guides/rabby",
         "guides/BIP-85",
-        "guides/sapling",
         "guides/social-recovery",
         "guides/step-by-step-guide",
         "guides/update-webview",
